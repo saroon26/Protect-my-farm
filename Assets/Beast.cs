8db6b423089447b5410f5fef0a95b8e7dc3ec12a
traineeNuha <109746723+traineeNuha@users.noreply.github.com>
2025-04-15 21:21:00 +0300

    added the beast

--- Assets/Beast.cs
+++ Assets/Beast.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class Beast : MonoBehaviour
 {
+    public GameObject Arther;
+    public float speed = 1f;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Arther = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(Arther.transform);
+        transform.Translate(transform.forward*speed *Time.deltaTime);
     }
 }
+
+
+
+// ÇÖÇİÉ ÓÑÚÉ ãÍÏÏÉ 
+// ÈÑíİÇÈ 
+// äÍÏÏ ÇáÇÊÌÇå ÚÔÇä ãÇ íÏæÑ 
+// ÇÖÇİÉ ÍÑßÇÊ ãËá ÇáÖÑÈ 
+
+
